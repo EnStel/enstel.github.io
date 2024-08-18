53c2e0be47c583f6e3ab5af3a3ad9bccaac6e253
EnStel <daixiki@gamil.com>
2024-08-18 21:39:40 +0800

    google analysis

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,23 @@
 import { defineUserConfig } from "vuepress";
-
 import theme from "./theme.js";
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics';
 
 export default defineUserConfig({
   base: "/",
+  
 
   lang: "zh-CN",
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
+
+  plugins: [
+    googleAnalyticsPlugin({
+      // 配置项
+      id: "G-4J93QM1PBG",      // 配置项
+    }),
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
